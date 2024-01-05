993b33d80f17d1223e8cb40ccd669a743a8a45bf
Johannes Lamberts <mail@j-lamberts.de>
2024-01-05 13:57:33 +0100

    fix: use openstreetmap

--- components/LeafletMap.vue
+++ components/LeafletMap.vue
@@ -33,7 +33,8 @@ const setupMap = () => {
   // add map background
   $leaflet
     .tileLayer(
-      "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png",
+      "https://tile.openstreetmap.org/{z}/{x}/{y}.png?",
+      // "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png",
       {
         attribution:
           '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
